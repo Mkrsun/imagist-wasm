cad0344d3e17cd4c836c72dc49287405d6b653ce
Manuel Martínez <mkrsun.dev@gmail.com>
2025-02-02 10:26:17 -0300

    fix: solving the issue!

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "🚀 Building WASM for Node.js..."
 wasm-pack build --target nodejs --out-dir pkg-node --no-pack
+rm -rf pkg-node/.gitignore
 
 echo "🌍 Building WASM for Web..."
 wasm-pack build --target web --out-dir pkg-web --no-pack
+rm -rf pkg-web/.gitignore
 
 echo "✅ Build completo!"
