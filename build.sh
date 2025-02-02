e35d60cca5aad9e53e1db12c1b8535346c2031b8
Manuel Martínez <mkrsun.dev@gmail.com>
2025-02-02 09:57:45 -0300

    fix: some fixes

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "🚀 Building WASM for Node.js..."
-wasm-pack build --target nodejs --out-dir pkg-node
+wasm-pack build --target nodejs --out-dir pkg-node --no-pack
 
 echo "🌍 Building WASM for Web..."
-wasm-pack build --target web --out-dir pkg-web
+wasm-pack build --target web --out-dir pkg-web --no-pack
 
 echo "✅ Build completo!"
