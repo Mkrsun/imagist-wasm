d4d7b7993372ccb2a1caed9683321e4c3a70584b
Manuel Martínez <mkrsun.dev@gmail.com>
2025-02-04 09:39:20 -0300

    feat: removing react native support because is unfeasible

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ echo "🚀 Building WASM for Node.js..."
 wasm-pack build --target nodejs --out-dir pkg-node --no-pack
 rm -rf pkg-node/.gitignore
 
-echo "🌍 Building WASM for Web..."
-wasm-pack build --target web --out-dir pkg-web --no-pack
-rm -rf pkg-web/.gitignore
+# echo "🌍 Building WASM for Web..."
+# wasm-pack build --target web --out-dir pkg-web --no-pack
+# rm -rf pkg-web/.gitignore
 
 echo "✅ Build completo!"
